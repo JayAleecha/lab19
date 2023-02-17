9147eb237173cd6b4b4f33632a45eba2d7e66d41
JayAleecha <aleecha.2546@gmail.com>
2023-02-17 18:00:02 +0700

    lab19_1

--- lab19_1.cpp
+++ lab19_1.cpp
@@ -20,20 +20,64 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names,vector<int> &scores,vector<char> &grades){
+    ifstream file(filename);
+    string L;
+    int one,two,three,sum = 0;
+    char g,n[27];
+    char format[] = "%[^:]: %d %d %d";
+    while(getline(file,L)){
+        sscanf(L.c_str(),format,n, &one,&two,&three);
+        names.push_back(n);
+        sum = one+two+three;
+        g = score2grade(sum);
+        scores.push_back(sum);
+        grades.push_back(g);
+    }
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &key){
+    string A;
+    char B[50];
+    char N[50];
+    cout << "Please input your command: ";
+    getline(cin,A);
+    sscanf(A.c_str(),"%s %[^]",B,N);
+        command = B;
+        key = N;
 }
 
-void searchName(){
-
+void searchName(vector<string> &names, vector<int> &scores, vector<char> &grades, string &key){
+    cout << "---------------------------------";
+    bool GG = false;
+    for(unsigned int i=0;i<names.size();i++){
+        if(toUpperStr(names[i])==key){
+            cout << names[i] << "'s score = " << scores[i];
+            cout << names[i] << "'s grade = " << grades[i];
+            GG=true;
+        }
+    }
+    if(!GG){
+        cout << "Cannot found.";
+    }
+    cout << "---------------------------------";    
 }
 
-void searchGrade(){
-
+void searchGrade(vector<string> &names, vector<int> &scores, vector<char> &grades, string &key){
+    cout << "---------------------------------";
+    bool GG = false;
+    string s;
+    for(unsigned int i=0;i<names.size();i++){
+        s=grades[i];
+        if(s==key){
+            cout << names[i] << " (" << scores[i] << ")";
+            GG=true;
+        }
+    }
+    if(!GG){
+        cout << "Cannot found.";
+    }
+    cout << "---------------------------------"; 
 }
 
 
